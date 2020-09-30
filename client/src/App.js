f6bcdb532bfe40ccd3d70e905c31b30316b5aac2
Ethan Clark <clark.ethan.r@gmail.com>
2020-09-29 20:59:46 -0600

    added posts and comments

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/shared/Home';
+import About from './components/shared/About';
+import Nomatch from './components/shared/Nomatch';
+import Navbar from './components/shared/Navbar';
+import Posts from './components/posts/Posts'; 
+import Comments from './components/comments/Comments';
+const App = () => (
+  <>
+    <Navbar />
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+      <Route exact path='/posts' component={Posts} />
+      <Route exact path='/posts/:id/comments' component={Comments} />
+      <Route component={Nomatch} />
+    </Switch>
+  </>
+)
+export default App;
